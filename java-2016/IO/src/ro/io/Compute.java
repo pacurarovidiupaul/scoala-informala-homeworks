a84ecf5f5516f8ca4dc7e00374bc07b442af5c8b
Ovidiu Pacurar <pacurarovidiupaul@live.com>
2016-11-01 15:48:37 +0200

    Little update!

--- java-2016/IO/src/ro/io/Compute.java
+++ java-2016/IO/src/ro/io/Compute.java
@@ -3,6 +3,11 @@ package ro.io;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Compute {
 
@@ -14,6 +19,13 @@ public class Compute {
 	EmployePosition position3 = EmployePosition.SALESOFFICER;
 
 	public void readAndCompute() throws IOException {
+		
+		Path path = Paths.get("sales-team.txt");
+		Charset charset = StandardCharsets.UTF_8;
+
+		String content = new String(Files.readAllBytes(path), charset);
+		content = content.replace("||", "|0|0");
+		Files.write(path, content.getBytes(charset));
 
 		File txt = new File("sales-team.txt");
 
